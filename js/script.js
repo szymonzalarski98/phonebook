a82bc8e08b4f562c04f1b1ce56530fb2bc7a3e77
Szymon Zalarski <szymon.zalarski@peritus-act.com>
2018-07-03 11:18:54 +0200

    v0.1

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// function addContact() {
+//   var telnumber = document.querySelector('#phoneNumber').value;
+//   var name = document.querySelector('#name').value;
+//   var lastName = document.querySelector('#lastName').value;
+//   var contactsContainer = document.querySelector(".contacts__container");
+//   var div = document.createElement("div");
+//   div.onclick = function() {
+//     this.parentElement.removeChild(this);
+//   };
+//   var node = document.createTextNode(telnumber + " " + name + " " + lastName);
+//   div.appendChild(node);
+//   contactsContainer.appendChild(div).className = "test";
+// }
+//
+// function formValidation() {
+//   var telnumber = document.querySelector('#telNumber').value;
+//   var name = document.querySelector('#name').value;
+//   var lastName = document.querySelector('#lastName').value;
+//   if(telnumber.length > 8 && name && lastName) {
+//     addContact(telnumber, name, lastName);
+//   } else {
+//     alert("Wrong!");
+//   }
+// }
+
+function addNewContact() {
+  addPhoneNumber();
+  addName();
+  addLastName();
+  addActions();
+}
+
+function addPhoneNumber() {
+var phoneInput = document.querySelector('#phoneNumber').value;
+var phoneCol = document.querySelector('#phoneCol');
+//create elements
+var phoneLi = document.createElement('li');
+var phoneNumber = document.createElement('span');
+phoneNumber.textContent = phoneInput;
+//append to document
+phoneLi.appendChild(phoneNumber);
+phoneCol.appendChild(phoneLi);
+}
+
+function addName() {
+  var nameInput = document.querySelector('#name').value;
+  var nameCol = document.querySelector('#nameCol');
+  var nameLi = document.createElement('li');
+  var name = document.createElement('span');
+  name.textContent = nameInput;
+  nameLi.appendChild(name);
+  nameCol.appendChild(nameLi);
+}
+
+function addLastName() {
+  var lastNameInput = document.querySelector('#lastName').value;
+  var lastNameCol = document.querySelector('#lastNameCol');
+  var lastNameLi = document.createElement('li');
+  var lastName = document.createElement('span');
+  lastName.textContent = lastNameInput;
+  lastNameLi.appendChild(lastName);
+  lastNameCol.appendChild(lastNameLi);
+}
+
+function addActions() {
+  var actionsCol = document.querySelector('#actionsCol');
+  var actionsLi = document.createElement('li');
+  var actionsButton = document.createElement('button');
+  actionsButton.textContent = "Delete";
+  actionsLi.appendChild(actionsButton);
+  actionsCol.appendChild(actionsLi);
+}
+
+function deleteRow() {
+
+}
